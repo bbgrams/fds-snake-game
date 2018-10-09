0839d522804e10195bcb506bfd7d935bcbfa721d
Shim Soyoung <sovv12@gmail.com>
2018-10-09 20:50:01 +0900

    로직 수정

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -6,54 +6,104 @@ import {ROWS, COLS} from './config';
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
   this.joints = [
-    { x: 5, y: 0 },
-    { x: 4, y: 0 },
-    { x: 3, y: 0 },
-    {x: 2, y: 0},
-    {x: 1, y: 0},
-    {x: 0, y: 0},
+    { x: 7, y: 1 },
+    { x: 6, y: 1 },
+    { x: 5, y: 1 },
+    { x: 4, y: 1 },
   ];
+  this.currentKey = 1;
 
   // 먹이의 좌표
   this.fruit = {x: 3, y: 5};
 }
+SnakeGameLogic.prototype.circle = function(x, y){
+  for ( let i = 1; i < this.joints.length; i++){
+    if ( this.joints[i].x === this.joints[0].x && this.joints[i].y === this.joints[0].y){
+      return true;
+    }
+  }
+  return false;
+}
+
+SnakeGameLogic.prototype.eat = function(){
+  if ( this.joints[0].x === this.fruit.x && this.joints[0].y === this.fruit.y){
+    return true;
+    return false;
+  }
+}
+
+SnakeGameLogic.prototype.setFruit = function(){
+  this.fruit.x = Math.floor(Math.random() * COLS );
+  this.fruit.y = Math.floor(Math.random() * ROWS );
+  if ( this.joints.some(item => item.x === this.fruit.x) &&
+       this.joints.some(item => item.y === this.fruit.y)){
+         this.setFruit();
+       }
+}
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
-  console.log('up');
+  this.currentKey = 0;
 }
 
 SnakeGameLogic.prototype.down = function() {
   // 아래쪽 화살표 키를 누르면 실행되는 함수
   console.log('down');
   // 배열안의 객체안의 숫자를 변경하고싶다.
-  // this.joints.push({x: 2, y:1})
-  // this.joints.shift();
-    // this.joints[0].y += 1
-    // const tail = this.joints.pop();
-    // tail.y = this.joints[0].y++;
-    // this.joints.unshift(tail);
+  this.currentKey = 2;
 }
 
 SnakeGameLogic.prototype.left = function() {
   // 왼쪽 화살표 키를 누르면 실행되는 함수
-  console.log('left');
+  this.currentKey = 3;
 }
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수
-  console.log('right');
-  const tail = this.joints.pop();
-  tail.x = this.joints[0].x + 1;
-  this.joints.unshift(tail);
+  this.currentKey = 1;
 }
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
-  console.log(`nextState`);
+  // console.log(`nextState`);
+  let tail;
+
+  if(this.eat()){
+    tail = this.joints.slice();
+    this.setFruit();
+  }else{
+    tail = this.joints.pop();
+  }
+
+  tail.x = this.joints[0].x;
+  tail.y = this.joints[0].y;
+
+  if ( this.currentKey === 0){
+    tail.y--;
+  }else if ( this.currentKey === 1){
+    tail.x++;
+  }else if ( this.currentKey === 2){
+    tail.y++;
+  }else if( this.currentKey === 3){
+    tail.x--;
+  }
+  this.joints.unshift(tail);
+
+  if( this.joints[0].x < 0 || this.joints[0].x >= COLS ||
+      this.joints[0].y < 0 || this.joints[0].y >= ROWS){
+        return false
+  }
+
+  if( this.circle()){
+    return false
+  }
+
+
   return true;
+  
+  
 }
 
 export default SnakeGameLogic;
